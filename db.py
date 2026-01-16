234f8fbd253c5d0b2fc8e10566d10621ed28664d
Bryce Miranda <brycemiranda20@gmail.com>
2026-01-16 21:52:05 +0530

    Connect db.py to environment variables

--- db.py
+++ db.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
 
 def get_db_connection():
+    # This looks for the variables you typed into Render
+    # If it can't find them (like on your laptop), it uses the "localhost" defaults
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="brycee2044",
-        database="chordify_audio"
+        host=os.environ.get('DB_HOST', 'localhost'),
+        user=os.environ.get('DB_USER', 'root'),
+        password=os.environ.get('DB_PASS', 'brycee2044'),
+        database=os.environ.get('DB_NAME', 'chordify_audio'),
+        port=os.environ.get('DB_PORT', '28995')
     )
